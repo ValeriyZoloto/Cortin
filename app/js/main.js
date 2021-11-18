9d6e502ac85d551e79a736fc3ce2296de3cb5aa6
Valeriy <leshikzoloto@gmail.com>
2021-11-18 18:36:56 +0300

    dealer desctop and mobile is ready

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,16 @@ $(function () {
 			//не выполнять
 		}
 	});
+
+	//input
+	$(".dealer__btn").on("click", function () {
+		var re = /^\d[\d\(\)\ -]{4,14}\d$/;
+		var myPhone = document.getElementById("phone").value;
+		var valid = re.test(myPhone);
+		if (valid) output = "Номер телефона введен правильно!";
+		else output = "Номер телефона введен неправильно!";
+		document.getElementById("message").innerHTML =
+			document.getElementById("message").innerHTML + "<br />" + output;
+		return valid;
+	});
 });
